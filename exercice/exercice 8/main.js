a99369825ebe59a909cd190f388ba952051984ce
Florian D <146062584+Domynux@users.noreply.github.com>
2023-09-28 10:47:08 +0200

    reussite a faire fonctionner mon programme de combat

--- exercice/exercice 8/main.js
+++ exercice/exercice 8/main.js
@@ -1,23 +1,56 @@
 class pokemon{
-    constructor(name,attack, defense, hp, luck){
+    constructor(name, attack, defense, hp, luck){
         this.name=name
         this.attack=attack
         this.defense=defense
         this.hp=hp
         this.isLuck=luck
     }
+
+    attackPokemon(pokemon) {
+         pokemon.hp -= pokemon.attack - pokemon.defense
+        }
+    }
    
-}
-
-
-let drakrai= new pokemon("Darkray",105, 90, 70,0.3 )
-let giratina= new pokemon("Giratina",120,100,150,0.5 )
-console.log(drakrai,giratina)
-
-function attackPokemon (poke1,poke2){
-    let dmg1 = poke1.attack-poke2.defense
-    let dmg2 = poke2.attack-poke1.defense
-}
 
 
 
+let darkrai = new pokemon("Darkray", 105, 90, 1700,0.5 )
+let giratina = new pokemon("Giratina", 1200,100,2500,0.1 )
+console.log(darkrai,giratina)
+
+
+
+while(darkrai.hp >0 && giratina.hp > 0 ){
+
+    var sentenceA= (darkrai.name + " a fait descendre les pv de son adversaire a " + giratina.hp)
+    var sentenceB= (giratina.name + " a fait descendre les pv de son adversaire a " + darkrai.hp)
+    if(Math.random()<darkrai.isLuck){
+        darkrai.attackPokemon(giratina)
+    
+    
+        console.log(sentenceA)
+        }
+        else{
+            console.log(darkrai.name+ " a raté son attaque")
+        }
+    if(giratina.hp <= 0){
+        console.log(giratina.name+ " est vancu ")
+        break
+    }
+    if(Math.random()<giratina.isLuck){
+        giratina.attackPokemon(darkrai)
+    
+    
+        console.log(sentenceB)
+        }
+        else{
+            console.log(giratina.name+ " a raté son attaque")
+        }
+    
+   
+    if(darkrai.hp <= 0 ){
+       console.log(darkrai.name+" est vancu")
+        break
+    }
+}   
