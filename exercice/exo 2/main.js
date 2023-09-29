b5c4feaf783c2293445ac4b8802dd998f80c79c2
Florian D <146062584+Domynux@users.noreply.github.com>
2023-09-29 21:18:05 +0200

    debut exo 2

---
+++ exercice/exo 2/main.js
@@ -0,0 +1,22 @@
+class killer{
+    constructor(name,hp){
+        this.name=name
+        this.hp=hp
+    }
+}
+let jason = new killer ("Jason the killer", 100)
+nameSurvivor = [ "Dr. dre","Cooler","Freezer", "Dimitri","Jean-Heude","Jean-luc"]
+function names(nameSurvivor){
+    var triage = nameSurvivor[Math.floor(Math.random()*nameSurvivor.length)]
+}
+    
+
+class survivor{
+    constructor(name, lckDeath, lckDodgeHit, lckDeathHit){
+        this.name=
+        this.lckdeath= lckDeath
+        this.lckDodgeHit = lckDodgeHit
+        this.lckDeathHit = lckDeathHit
+    }
+}
+names(nameSurvivor)
