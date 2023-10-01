e23dc428d50738f997bf06970e43f1e2687825b8
Florian D <146062584+Domynux@users.noreply.github.com>
2023-10-01 22:23:23 +0200

    uptade exo 2

--- exercice/exo 2/main.js
+++ exercice/exo 2/main.js
@@ -6,29 +6,53 @@ class killer{
 }
 let jason = new killer ("Jason the killer", 100)
 
-let nameSurvivor = [ "Dr. dre","Cooler","Freezer", "Dimitri","Jean-Heude","Jean-luc"]
+let perkSurvivor = [ "chad", "simp","gameur","depresif","fumeur","drogé"]
+let nameSurvivor= ["DR.dre","Alexy","Louis","Alexis","Keanay","Florian", "Endrou","Jean-luke","Emmanuel","Leonardo","Yanis","Leo"]
+var nameTirage = nameSurvivor[Math.floor(Math.random()*nameSurvivor.length)]
+
+var tirage = perkSurvivor[Math.floor(Math.random()*perkSurvivor.length)]
+
+stat(statTirage){
+    if(statTirage==="chad"){
+        this.lckDeathHit=0.3
+        this.lckDodgeHit=0.7
+        this.lckDeath=0.2
+    }
+    if(statTirage==="simp"){
+        this.lckDeathHit=0.5
+        this.lckDodgeHit=0.7
+        this.lckDeath=0.2
+    }
+    if(statTirage==="gameur"){
+        this.lckDeathHit=0.6
+        this.lckDodgeHit=0.5
+        this.lckDeath=0.4
+    }
+    if(statTirage===""){
+        this.lckDeathHit=0.
+        
+    }
 
 
-    var tirage = nameSurvivor[Math.floor(Math.random()*nameSurvivor.length)]
-
-    
+}
 
 class survivor{
-    constructor(name, lckDeath, lckDodgeHit, lckDeathHit){
-        this.name=names
-        this.lckdeath= lckDeath
-        this.lckDodgeHit = lckDodgeHit
-        this.lckDeathHit = lckDeathHit
+    constructor(){
+        this.name=nameTirage
+        this.perk=tirage
+        this.lckDeath= stat(statTirage)
+        this.lckDodgeHit = stat(statTirage)
+        this.lckDeathHit = stat(statTirage)
     }
 }
 
+survivorOne=new survivor()
 
-while(survivor==0 || jason.hp == 0){
+
+
+while (jason.hp == 0){
   
-    console.log(tirage+"est un survivent"  )
-    console.log(tirage+"est un survivent"  )
-    console.log(tirage+"est un survivent"  )
-    console.log(tirage+"est un survivent"  )
+    
 break
 
 }
